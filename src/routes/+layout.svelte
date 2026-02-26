8a386e90fe3a8636c4dab90aa4d6c6aee11b4900
Andrew Riker <andrew.riker21@gmail.com>
2026-02-25 22:41:06 -0500

    Layout header and footer sections

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,21 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<header class="absolute top-0 left-0 w-full p-8 flex justify-between">
+	<div>
+		<div class="w-12 h-12 border rounded-lg"></div>
+	</div>
+	<div class="flex gap-8">
+		<div class="w-12 h-12 border rounded-lg"></div>
+		<div class="w-12 h-12 border rounded-lg"></div>
+		<div class="w-12 h-12 border rounded-lg"></div>
+		<div class="w-12 h-12 border rounded-lg"></div>
+	</div>
+</header>
+
 {@render children()}
+
+<footer class="w-full min-h-[15vh] bg-[var(--color-surface)]">
+
+</footer>
