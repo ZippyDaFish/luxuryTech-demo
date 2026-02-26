148f894efc79cf054d2ed3d82720e4d0129ac20b
Andrew Riker <andrew.riker21@gmail.com>
2026-02-25 22:15:50 -0500

    Style headers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- Hero -->
+<section class="min-h-screen flex flex-col md:flex-row">
+    <!-- Hero Text -->
+    <div class="md:w-1/2 w-full flex items-center justify-center bg-[var(--color-bg]">
+        <div class="w-full max-w-xl px-6">
+            <h1 class="text-6xl mb-8">Luxury Tech Stuff</h1>
+            <h3 class="text-lg mb-8">Some catchy subtitle.</h3>
+            <a href="#products" class="inline-block btn-primary px-8 py-3 rounded-[var(--radius-md)] 
+                font-semibold transition-all duration-200 ease-out hover:bg-[var(--color-primary-hover)] 
+                hover:-translate-y-0.5 hover:shadow-md active:translate-y-0 active:shadow-sm">
+                See Our Products
+            </a>
+        </div>
+    </div>
+    <!-- Hero Media -->
+    <div class="md:w-1/2 w-full bg-[var(--color-bg)] h-screen overflow-hidden">
+        <!-- Add something here -->
+    </div>
+</section>
